2e2c4e740c4c64f94c900a7fb9d9ff4f3cc40970
rthomas86 <rebkthomas72@gmail.com>
2025-06-02 17:35:42 -0700

    completed the resume page with proficiencies and a download of my resume.

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { frontEnd, backEnd } from "./assets/scripts/proficiencies.js";
+import "./assets/styles/resume.css";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <div className="container">
+      <div className="User_Body">
+        <div className="title">Resume</div>
+      </div>
+      <div className="Resume_Container">
+        {/*download resume*/}
+        <div>
+          Download my{" "}
+          <a href="/docs/RichardThomas.docx" download>
+            resume
+          </a>
+        </div>
+        {/*unordered list loop through the front-end and back-end proficiencies
+        and display them in the list*/}
+        <div className="Proficiency_Title">Front-End Proficiencies</div>
+
+        <ul>
+          {frontEnd.map((item) => (
+            <li className="Proficiency_Item" key={item.id}>
+              {item.proficiency}
+            </li>
+          ))}
+        </ul>
+        <div className="Proficiency_Title">Back-End Proficiencies</div>
+        <ul>
+          {backEnd.map((item) => (
+            <li className="Proficiency_Item" key={item.id}>
+              {item.proficiency}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
---
+++ src/pages/resume/assets/scripts/proficiencies.js
@@ -0,0 +1,61 @@
+export const frontEnd = [
+  {
+    id: 1,
+    proficiency: "HTML",
+  },
+  {
+    id: 2,
+    proficiency: "CSS",
+  },
+  {
+    id: 3,
+    proficiency: "JavaScrip",
+  },
+  {
+    id: 4,
+    proficiency: "JQuery",
+  },
+  {
+    id: 5,
+    proficiency: "Responsive Design",
+  },
+  {
+    id: 6,
+    proficiency: "React",
+  },
+  {
+    id: 7,
+    proficiency: "Bootstrap",
+  },
+];
+
+export const backEnd = [
+  {
+    id: 1,
+    proficiency: "APIs",
+  },
+  {
+    id: 2,
+    proficiency: "Node",
+  },
+  {
+    id: 3,
+    proficiency: "Express",
+  },
+  {
+    id: 4,
+    proficiency: "MySQL, Sequelize",
+  },
+  {
+    id: 5,
+    proficiency: "MongoDB, Mongoose",
+  },
+  {
+    id: 6,
+    proficiency: "Rest",
+  },
+  {
+    id: 7,
+    proficiency: "GraphQL",
+  },
+];
